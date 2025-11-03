e4b4f040f2c97555f5df01b6a9811c1b93ca2823
mure <surmanmar@alu.edu.gva.es>
2025-11-03 13:33:08 +0100

    cambio1 (#1)
    Co-authored-by: mure <sureshmanghanmalmarti@gmail.com>

--- site.js
+++ site.js
@@ -1,2 +1,2 @@
-var userName = prompt(What is your name?); 
-alert(Hello,  + userName + ! Welcome to the world of JavaScript.);
+var userName = prompt("Cuál es tu nombre?"); 
+alert(Hola,  + userName + ! Bienvenido al mundo de JavaScript.);
